6f39040fe7d417bf762978603d2bcc7e79ef1bf2
newakrish <newakrish@github.com>
2026-01-03 15:27:07 +0545

    patch added

--- backend/route_post_patch.py
+++ backend/route_post_patch.py
@@ -40,3 +40,43 @@ def create_project():
         return jsonify(new_project.to_json()), 201
     except Exception as e:
         return jsonify({"error": str(e)}), 400
+
+# Patch Route to update an existing project
+@pp_bp.route('/projects/<int:project_id>', methods=['PATCH'])
+def update_project(project_id):
+    data = request.get_json()
+    project = Project.query.get(project_id)
+    if not project:
+        return jsonify({"error": "Project not found."}), 404
+
+    # Update status if provided
+    if 'status' in data:
+        if data['status'] not in allowed_status:
+            return jsonify({"error": f"Invalid status value. Must be one of: {allowed_status}"}), 400
+        project.status = data['status']
+
+    # Update total budget if provided
+    if 'budget' in data:
+        try:
+            project.budget = float(data['budget'])
+        except ValueError:
+            return jsonify({"error": "Budget must be a number"}), 400
+    
+    # Update 'project_manager' if provided
+    if 'project_manager' in data:
+        project.project_manager = data['project_manager']
+    
+    # Update 'deadline' if provided
+    if 'deadline' in data:
+        try:
+            project.deadline = datetime.strptime(data['deadline'],'%Y-%m-%d').date()
+        except ValueError:
+            return jsonify({"error": "Invalid date format. Use YYY-MM-DD"}),400
+
+    # Commit changes
+    try:
+        db.session.commit()
+        return jsonify(project.to_json()), 200
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({"error": str(e)}), 400
