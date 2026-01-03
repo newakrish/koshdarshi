e4b4d63bb602518fbe10cdbc472f52d6567a5a95
Ishan <ishannakarmi3@gmail.com>
2026-01-03 15:32:59 +0545

    Add koshdarshi project files properly

---
+++ koshdarshi/backend/models.py
@@ -0,0 +1,20 @@
+from config import db
+
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    project_name = db.Column(db.String(100),unique=False, nullable=False)
+    description = db.Column(db.String(255),unique=False, nullable=True)
+    budget = db.Column(db.Float,unique=False, nullable=False)
+    deadline = db.Column(db.Date,unique=False, nullable=False)
+    address = db.Column(db.String(255), unique=False, nullable=False)
+    project_manager = db.Column(db.String(100), unique=False, nullable=False)   
+    def to_json(self):
+        return {
+            "id": self.id,
+            "project_name": self.project_name,
+            "description": self.description,
+            "budget": self.budget,
+            "deadline": self.deadline,
+            "address": self.address,
+            "project_manager": self.project_manager
+        }
